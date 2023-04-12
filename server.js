66f2788e9198a4c0a643c56015272f3e2ed09b5e
s0a1e2 <oops2198@gmail.com>
2023-04-13 02:50:42 +0330

    fix send and receive data with web socket

--- server.js
+++ server.js
@@ -9,30 +9,6 @@ app.set('view engine', 'ejs');
 
 const wss = new WebSocket.Server({port: 8080});
 
-let mobile;
-
-wss.on('connection', function connection(ws) {
-    console.log(`this is ws argument : ${ws}`);
-        ws.on('message', function incoming(data) {
-        console.log(`this is data argument : ${data}`);
-        data = JSON.parse(data);
-        mobile = JSON.parse(data.mobile);
-        password = JSON.parse(data.password);
-        if(data.mobile === '12345678910' && data.password === 'password') {
-            ws.send(JSON.stringify({success: true}));
-            ws.close();
-        } else {
-            ws.send(JSON.stringify({
-                success: false,
-                message: 'Invalid mobile or password.'
-            }));
-        }
-    });
-    ws.send(JSON.stringify({
-        mobile: mobile.value,
-        password: password.value
-    }));
-});
 
 app.get('/', (req, res) => {
     res.render('login');
@@ -44,6 +20,17 @@ app.get('/signup', (req, res) => {
 
 app.get('/messaging', (req, res) => {
     res.render('messaging');
+});
+wss.on('connection', (ws) => {
+  console.log('ws');
+  ws.on('message', (msg) => {
+    let data = JSON.parse(msg);
+    if(data.mobile === '12345678910' && data.password === 'pass123') {
+      ws.send('true');
+    } else {
+      ws.send('false');
+    }
+  });
 });
 
 app.listen(port, () => {
