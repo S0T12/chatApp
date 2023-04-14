3886b0722f622456d549ac219c930033e119f9cf
s0a1e2 <oops2198@gmail.com>
2023-04-14 19:27:36 +0330

    add database and complete signup

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const path = require('path');
 const WebSocket = require('ws');
+const database = require('./database');
 const app = express();
 
 const port = process.env.port || 3000;
@@ -8,7 +8,44 @@ const port = process.env.port || 3000;
 app.set('view engine', 'ejs');
 
 const wss = new WebSocket.Server({port: 8080});
+const wss1 = new WebSocket.Server({port: 8081});
+// login
+wss.on('connection', (ws) => { 
+  ws.on('message', (msg) => {
+    let data = JSON.parse(msg);
+      
+    database.query(`SELECT * FROM users WHERE mobile=${data.mobile} AND password='${data.password}'`, (err, result) => {
+      if(err) throw err;
+      
+      if(result.length > 0) {
+        ws.send('true');
+      } else {
+        ws.send('false');
+      }
+    });
+  });
+});
 
+// register
+wss1.on('connection', (ws) => {
+  ws.on('message', (msg) => {
+    let data = JSON.parse(msg);
+    
+    database.query(`SELECT * FROM users WHERE mobile=${data.mobile}`, (err, result) => {
+      if(err) throw err;
+      
+      if(result.length > 0) {
+        ws.send('false');
+      } else {
+        database.query(`INSERT INTO users (mobile, password) VALUES (${data.mobile}, '${data.password}')`, (err, result) => {
+          if(err) throw err;
+          
+          ws.send('true');
+        });
+      }
+    });
+  });
+});
 
 app.get('/', (req, res) => {
     res.render('login');
@@ -20,17 +57,6 @@ app.get('/signup', (req, res) => {
 
 app.get('/messaging', (req, res) => {
     res.render('messaging');
-});
-wss.on('connection', (ws) => {
-  console.log('ws');
-  ws.on('message', (msg) => {
-    let data = JSON.parse(msg);
-    if(data.mobile === '12345678910' && data.password === 'pass123') {
-      ws.send('true');
-    } else {
-      ws.send('false');
-    }
-  });
 });
 
 app.listen(port, () => {
