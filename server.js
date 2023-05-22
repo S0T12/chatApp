2534dd90f135268225b9e977276d150c1bc570b2
s0a1e2 <oops2198@gmail.com>
2023-05-22 19:51:00 +0330

    complete login and regsiter pages

--- server.js
+++ server.js
@@ -11,127 +11,128 @@ const secretOrPrivateKey = 'helloWorld';
 
 app.set('view engine', 'ejs');
 
-const wss = new WebSocket.Server({port: 8080});
-const wss1 = new WebSocket.Server({port: 8081});
-const wss2 = new WebSocket.Server({port: 8082});
-const wss3 = new WebSocket.Server({port: 8083});
+const wss = new WebSocket.Server({ port: 8080 });
 
-// login
 wss.on('connection', (ws) => {
   ws.on('message', (msg) => {
     let data = JSON.parse(msg);
 
-    console.log(`give data from client in server : ${data.mobile}`);
-    
-    database.query(`SELECT * FROM users WHERE mobile=?`, [data.mobile], (err, result) => {
-      if (err) throw err;
+    switch (data.action) {
+      // login request
+      case 'login':
+        database.query(`SELECT * FROM users WHERE mobile=? and password=?`, [data.mobile, data.password], (err, result) => {
+          if (err) throw err;
 
-      console.log(`this is result.length : ${result.length}`);
-      if (result.length > 0) {
-        const uuid = result[0].uuid;
-        let token = jwt.sign({
-          mobile: data.mobile,
-          uuid: uuid
-        }, secretOrPrivateKey, {
-          expiresIn: '7d'
+          if (result.length > 0) {
+            const uuid = result[0].uuid;
+            let token = jwt.sign({
+              mobile: data.mobile,
+              uuid: uuid,
+              }, secretOrPrivateKey, {
+              expiresIn: '7d'
+            });
+            
+            let info = {
+              token: token,
+              action: "token"
+            }
+            ws.send(JSON.stringify(info));
+
+          } else {
+            ws.send('false');
+          }
         });
-        console.log(`send token from server : ${token}`);
-        ws.send(token);
-
-      } else {
-        ws.send('false');
-      }
-    });
-  });
-});
-
-// JWT verification
-wss3.on('connection', (ws) => {
-    ws.on('message', (msg) => {
-      let data = JSON.parse(msg);
-      console.log('this msg for JWT verification : ' + data);
-      let result;
-      try {
-        result = jwt.verify(data, secretOrPrivateKey);
-      } catch(err) {
-        console.log(err);
-        ws.send('false');
-        console.log(`data is not create in server for JWT verification`);
-        return;
-      }
-      if (result) {
-        console.log(`data have value`);
-        ws.send('true');
-      } else {
-        console.log(`data not have value`);
-        ws.send('false');
-      }
-    });
-});
-
-
-// register
-wss1.on('connection', (ws) => {
-  ws.on('message', (msg) => {
-    let data = JSON.parse(msg);
-    console.log('hello world');
-    
-    let uuidValue = uuid.v4();
-    
-    database.query(`SELECT * FROM users WHERE mobile=${data.mobile}`, (err, result) => {
-      if(err) throw err;
+        break;
       
-      if(result.length > 0) {
-        ws.send('false');
-      } else {
-        database.query(`INSERT INTO users (mobile, password, uuid) VALUES (${data.mobile}, '${data.password}', '${uuidValue}')`, (err, result) => {
+      // register request
+      case 'register':
+        let uuidValue = uuid.v4();
+    
+        database.query(`SELECT * FROM users WHERE mobile=${data.mobile}`, (err, result) => {
           if(err) throw err;
           
-          ws.send('true');
+          if(result.length > 0) {
+            ws.send('false');
+          } else {
+            database.query(`INSERT INTO users (mobile, password, uuid) VALUES (${data.mobile}, '${data.password}', '${uuidValue}')`, (err, result) => {
+              if(err) throw err;
+              
+              ws.send('true');
+            });
+          }
         });
-      }
-    });
-  });
-});
+        break;
 
-// Add contact
-wss2.on('connection', (ws) => {
-  ws.on('message', (msg) => {
-    let data = JSON.parse(msg);
-    let token = data.token;
-    
-    jwt.verify(token, secretOrPrivateKey, (err, decoded) => {
-      if (err) {
-        ws.send('invalid token');
-        return;
-      }
-      
-      let mobile = decoded.id;
-      let uuid = decoded.uuid;
-      let name = data.name;
-      let number = data.number;
-      
-      database.query(`INSERT INTO contacts (contact_name, contact_mobile, user_mobile, user_uuid) VALUES ('${name}', '${number}', '${mobile}', '${uuid}')`, (err, result) => {
-        if (err) throw err;
+      // messaging request
+      case 'messaging':
+        let token = data.token;
         
-        ws.send('contact added');
+        jwt.verify(token, secretOrPrivateKey, (err, decoded) => {
+          if (err) {
+            ws.send('invalid token');
+            return;
+          }
+          
+          let mobile = decoded.id;
+          let uuid = decoded.uuid;
+          let name = data.name;
+          let number = data.number;
+          
+          database.query(`INSERT INTO contacts (contact_name, contact_mobile, user_mobile, user_uuid) VALUES ('${name}', '${number}', '${mobile}', '${uuid}')`, (err, result) => {
+            if (err) throw err;
+            
+            ws.send('contact added');
+          });
       });
-    });
+      break;
+
+      // jwt verification request
+      case 'jwtVerify':
+        let result;
+        try {
+          result = jwt.verify(data.token, secretOrPrivateKey);
+        } catch(err) {
+          console.log(err);
+
+          let data = {
+            result: "false",
+            action: "jwt"
+          }
+          ws.send(JSON.stringify(data));
+          return;
+        }
+
+        if (result) {
+          let data = {
+            result: "true",
+            action: "jwt"
+          }
+          ws.send(JSON.stringify(data));
+        } else {
+          let data = {
+            result: "false",
+            action: "jwt"
+          }
+          ws.send(JSON.stringify(data));
+        }
+        break;
+    }
+
   });
 });
 
 app.get('/', (req, res) => {
-    res.render('login');
+  res.render('login');
 });
 
 app.get('/signup', (req, res) => {
-    res.render('register');
+  res.render('register');
 });
 
-app.get('/messaging', (req, res) => {
-    res.render('messaging');
+app.get('/messaging/:uuid', (req, res) => {
+  res.render('messaging');
 });
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+  console.log(`server is running on port ${port}`);
 });
