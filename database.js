341bc1f61ffc4c3f77d7cf1759b83a61b61dc2ce
amirhossin_shahsavan <amirhosinshahsavan@gmail.com>
2023-05-28 00:58:19 +0330

    fix action contact in messaging

--- database.js
+++ database.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '',
+    user: 'mir',
+    password: 'amir002051',
     database: 'chatapp'
 });
 
