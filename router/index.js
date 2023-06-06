282a7cc023e6ac5fb4367fd7c87d9e16833325fb
s0a1e2 <oops2198@gmail.com>
2023-06-06 18:29:14 +0330

    code modularization

---
+++ router/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.port || 3000;
+
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('login');
+});
+
+app.get('/signup', (req, res) => {
+  res.render('register');
+});
+
+app.get('/messaging/', (req, res) => {
+  res.render('messaging');
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
