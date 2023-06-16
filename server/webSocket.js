342dff59b7087e9fa932c5db790516a7b35d3466
s0a1e2 <oops2198@gmail.com>
2023-06-16 05:34:20 +0330

    add edit contact name

--- server/webSocket.js
+++ server/webSocket.js
@@ -227,7 +227,53 @@ module.exports = function startWebsocketServer () {
             });
           });
           break;
-  
+          
+          // update contact name
+          case 'updateContactName':
+            jwt.verify(data.token, secretOrPrivateKey, (err, decoded) => {
+              if (err) {
+                ws.send('invalid token');
+                return;
+              }
+    
+              const mobile = decoded.mobile;
+              const contactMobile = data.contactMobile;
+              const newName = data.newName;
+    
+              database.query(
+                `UPDATE contacts SET contact_name = '${newName}' WHERE user_mobile = '${mobile}' AND contact_mobile = '${contactMobile}'`,
+                (err, result) => {
+                  if (err) throw err;
+
+                  if (result.affectedRows > 0) {
+                    database.query(`SELECT contact_name, contact_mobile FROM contacts WHERE user_mobile='${mobile}'`, (err, results) => {
+                      console.log(results);
+                      if (err) {
+                        console.log(err);
+                        ws.send(JSON.stringify({
+                          type: 'contacts',
+                          contacts: []
+                        }));
+                        return;
+                      }
+                    
+                      let contacts = results.map(function(result) {
+                        return {
+                          name: result.contact_name,
+                          number: result.contact_mobile
+                        }
+                      });
+                      
+                      ws.send(JSON.stringify({
+                      type: 'contacts',
+                      contacts: contacts
+                      }));
+                    });
+                  }
+                }
+              );
+            });
+            break;
   
         // jwt verification request
         case 'jwtVerify':
